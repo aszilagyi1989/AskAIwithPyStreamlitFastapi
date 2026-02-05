07212933f388b7d56b6477702dc70b44a545f61d
Szilágyi Ádám <sz.adam1989@gmail.com>
2026-02-05 08:11:21 +0100

    upload 2

--- frontend/app.py
+++ frontend/app.py
@@ -28,67 +28,67 @@ st.set_page_config(
   )
 
 
-REDIRECT_URI = st.secrets["REDIRECT_URI"]
-# COOKIE_SECRET = st.secrets["COOKIE_SECRET"]
-CLIENT_ID = st.secrets["CLIENT_ID"]
-CLIENT_SECRET = st.secrets["CLIENT_SECRET"]
-# SERVER_METADATA_URL = st.secrets["SERVER_METADATA_URL"]
-AUTHORIZE_URL = "https://accounts.google.com"
-TOKEN_URL = "https://oauth2.googleapis.com"
-REVOKE_URL = "https://oauth2.googleapis.com"
-
-oauth2 = OAuth2Component(CLIENT_ID, CLIENT_SECRET, AUTHORIZE_URL, TOKEN_URL, TOKEN_URL, REVOKE_URL)
-
-st.set_page_config(page_title = "Google Auth App", page_icon = "🔐")
-
-if "auth" not in st.session_state:
-  result = oauth2.authorize_button(
-    name = "Bejelentkezés Google-lel",
-    icon = "https://www.google.com",
-    redirect_uri = REDIRECT_URI,
-    scope = "openid email profile",
-    key = "google_auth",
-    use_container_width = True
-  )
-
-  if result:
-    
-    id_token = result.get("token", {}).get("id_token")
-    payload = id_token.split('.')[1]
-    user_info = json.loads(base64.urlsafe_b64decode(payload + "==").decode("utf-8"))
-        
-    st.session_state.auth = result
-    st.session_state.user_info = user_info
-    st.rerun()
-
-  st.stop()
-
-  user = st.session_state.user_info
-  st.sidebar.image(user.get("picture"), width = 50) # Profilkép
-  st.sidebar.write(f"Greetings, **{user.get('name')}**!")
-  st.sidebar.write(f"📧 {user.get('email')}")
-  st.success("Sikeresen bejelentkeztél!")
-  if st.button("Kijelentkezés"):
-    for key in ["auth", "user_info"]:
-      if key in st.session_state:
-        del st.session_state[key]
-    st.rerun()
-
-  st.write("Adataid:", user)
-
-# if not st.user.is_logged_in:
-#   st.info("If you have got an OpenAI API Key, then after sign in with Google you can use this web application with different AI models to chat and create photos and videos.")
-#   st.info("You can buy credits here: https://platform.openai.com/settings/organization/billing/overview")
-#   st.info("Your email address and other data like your questions and descriptions are saved into PostgreSQL database tables.")
-#   st.info("Your AI generated photos and videos are stored on Amazon locally.")
-#   if st.button("Login with Google"):
-#     st.login()
-#   st.stop()
-# else:
-#   st.write(f"Greetings, {st.user.name}!")
+# REDIRECT_URI = st.secrets["REDIRECT_URI"]
+# # COOKIE_SECRET = st.secrets["COOKIE_SECRET"]
+# CLIENT_ID = st.secrets["CLIENT_ID"]
+# CLIENT_SECRET = st.secrets["CLIENT_SECRET"]
+# # SERVER_METADATA_URL = st.secrets["SERVER_METADATA_URL"]
+# AUTHORIZE_URL = "https://accounts.google.com"
+# TOKEN_URL = "https://oauth2.googleapis.com"
+# REVOKE_URL = "https://oauth2.googleapis.com"
+# 
+# oauth2 = OAuth2Component(CLIENT_ID, CLIENT_SECRET, AUTHORIZE_URL, TOKEN_URL, TOKEN_URL, REVOKE_URL)
+# 
+# st.set_page_config(page_title = "Google Auth App", page_icon = "🔐")
+# 
+# if "auth" not in st.session_state:
+#   result = oauth2.authorize_button(
+#     name = "Bejelentkezés Google-lel",
+#     icon = "https://www.google.com",
+#     redirect_uri = REDIRECT_URI,
+#     scope = "openid email profile",
+#     key = "google_auth",
+#     use_container_width = True
+#   )
+# 
+#   if result:
 #     
-#   if st.button("Logout"):
-#     st.logout()
+#     id_token = result.get("token", {}).get("id_token")
+#     payload = id_token.split('.')[1]
+#     user_info = json.loads(base64.urlsafe_b64decode(payload + "==").decode("utf-8"))
+#         
+#     st.session_state.auth = result
+#     st.session_state.user_info = user_info
+#     st.rerun()
+# 
+#   st.stop()
+# 
+#   user = st.session_state.user_info
+#   st.sidebar.image(user.get("picture"), width = 50) # Profilkép
+#   st.sidebar.write(f"Greetings, **{user.get('name')}**!")
+#   st.sidebar.write(f"📧 {user.get('email')}")
+#   st.success("Sikeresen bejelentkeztél!")
+#   if st.button("Kijelentkezés"):
+#     for key in ["auth", "user_info"]:
+#       if key in st.session_state:
+#         del st.session_state[key]
+#     st.rerun()
+# 
+#   st.write("Adataid:", user)
+
+if not st.user.is_logged_in:
+  st.info("If you have got an OpenAI API Key, then after sign in with Google you can use this web application with different AI models to chat and create photos and videos.")
+  st.info("You can buy credits here: https://platform.openai.com/settings/organization/billing/overview")
+  st.info("Your email address and other data like your questions and descriptions are saved into PostgreSQL database tables.")
+  st.info("Your AI generated photos and videos are stored on Amazon locally.")
+  if st.button("Login with Google"):
+    st.login()
+  st.stop()
+else:
+  st.write(f"Greetings, {st.user.name}!")
+
+  if st.button("Logout"):
+    st.logout()
 
 st.title('Ask AI with Python', anchor = False, help = None)
 password = st.text_input('Set your OpenAI API key:', type = 'password', value = os.environ['OPENAI_API_KEY'], placeholder = "If you don't have one, then you can create here: https://platform.openai.com/api-keys", key = "my_key") 
